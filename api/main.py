21fbe78b3ff8a65c57b66c78167fd45d7e298593
mike <michaelwilsonsj@hotmail.com>
2023-05-07 13:53:54 -0400

    switched to XGboost instead of elo

--- api/main.py
+++ api/main.py
@@ -1,62 +1,21 @@
-from fastapi import FastAPI
-import pandas as pd
-import os
+import httpx
+import asyncio
+from fastapi import FastAPI, BackgroundTasks
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup 
 
-app = FastAPI()
+#TODO
+# calc elo function
+#
+#TODO
+# update elo function 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-CSV_FILE = os.path.join(DATA_DIR, 'mlb_elo.csv')
+elo_ratings = {}
 
-def calculate_elo(home_elo, away_elo, home_score, away_score, k=20):
-    home_expected = 1 / (1 + 10 ** ((away_elo - home_elo) / 400))
-    away_expected = 1 / (1 + 10 ** ((home_elo - away_elo) / 400))
+async def fetch_game_data(date):
+    # fetch game data function
+    #
 
-    if home_score > away_score:
-        home_actual = 1
-        away_actual = 0 
-    elif home_score < away_score:
-        home_actual = 0
-        away_actual = 1
-    else:
-        home_actual = 0.5
-        away_actual = 0.5
-
-    new_home_elo = home_elo + k * (home_actual - home_expected)
-    new_away_elo = away_elo + k * (home_actual - home_expected)
-
-    return new_home_elo, new_away_elo
-
-@app.get("/api/elo")
-async def get_elo_ratings():
-   df = pd.read_csv(CSV_FILE) 
-   teams = df['team'].unique()
-   ratings = {team: 1500 for team in teams}
-
-   for index, row in df.iterrows():
-       home_team, away_team = row['team'], row['opp']
-       home_score, away_score = row['score'], row['opp_score']
-       home_elo, away_elo = ratins[home_team], ratings[away_team]
-       new_home_elo, new_away_elo = calculate_elo(home_elo, away_elo,
-               home_score, away_score)   
-       ratings[home_team] = new_home_elo
-       ratings[away_team] = new_away_elo
-
-    return [{"team": team, "rating": rating} for team, rating in
-            ratings.items()]
-
-
-       
-
-
-
-
-
-
-
-
-
-
-
-
-
+async def fetch_calc_historical_ratings():
+    today = datetime.utcnow().date()
+    current_year = today.year
